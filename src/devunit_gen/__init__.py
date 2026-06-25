a2f8a93a4fc435fbc429b97c5c2a595d0638c0f0
brian-leanrs <epitome-tampers.1v@icloud.com>
2026-06-25 08:20:17 -0700

    target script plumbed into testafize

--- src/devunit_gen/__init__.py
+++ src/devunit_gen/__init__.py
@@ -1,2 +1,1 @@
-def main() -> None:
-    print("Hello from devunit-gen!")
+from .devunit_gen import main as main
---
+++ src/devunit_gen/__main__.py
@@ -0,0 +1,6 @@
+import sys
+
+from .devunit_gen import main
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
